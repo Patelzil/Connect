31b80d406b7f792325f98dc58685070f5db84c66
Patelzil <58107277+Patelzil@users.noreply.github.com>
2020-03-02 20:33:10 -0600

    Board size generated and runGame process written as comments.

--- src/MyGame.java
+++ src/MyGame.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class MyGame implements GameLogic
 {
     private AIPlayer computer;
@@ -14,7 +16,17 @@ public class MyGame implements GameLogic
      */
     public void runGame()
     {
+        // randomly generates the board size(btn 6-12)
+        // and send the information to the players
+        int size = generateBoardSize();
+        computer.setInfo(size, this);
+        human.setInfo(size,this);
 
+        // todo: determines which player starts the game
+
+        // todo: alternate between the players
+
+        // todo:determine if the player wins or if there is a draw and then inform the player
     }// end runGame
 
     /* setAnswer
@@ -24,6 +36,18 @@ public class MyGame implements GameLogic
      */
     public void setAnswer(int col)
     {
-
+        // todo: write code
     }// setAnswer
+
+    /* generateBoardSize
+     * Purpose - randomly generates the board size and returns it
+     *
+     */
+    private int generateBoardSize()
+    {
+        int max = 12;
+        int min = 6;
+        Random ran = new Random();
+        return ( ran.nextInt((max-min) + 1) + min );
+    }// end generateBoardSize
 }// class MyGame
