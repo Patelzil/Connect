bd12df57152480e19ff2b1e6836d1eb251b98b33
Patelzil <58107277+Patelzil@users.noreply.github.com>
2020-03-02 20:08:10 -0600

    Set the main class up to run the game.

--- src/A3Main.java
+++ src/A3Main.java
@@ -2,6 +2,11 @@ public class A3Main
 {
     public static void main(String[] args)
     {
+        System.out.println("The game starts...");
 
+        MyGame theGame = new MyGame();
+        theGame.runGame();
+
+        System.out.println("End of Processing");
     }// end main
 }// class A3Main
