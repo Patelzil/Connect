de8d2c90202b309e1fc6583bee670263556ebafa
Patelzil <58107277+Patelzil@users.noreply.github.com>
2020-03-12 17:29:41 -0500

    Added private method playColumn that chooses the column the AI will play at

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -10,10 +10,7 @@ public class AIPlayer implements Player
      */
     public void lastMove(int col)
     {
-        int num = 0; // num chosen by the AI to play
-
-        //todo: randomly select column where the AI will play
-
+        int num = playColumn(col);
         game.setAnswer(num);
     }// end lastMove
 
@@ -24,7 +21,7 @@ public class AIPlayer implements Player
      */
     public void gameOver(Status winner)
     {
-
+        // todo: write code
     }// end gameOver
 
 
@@ -40,4 +37,16 @@ public class AIPlayer implements Player
         boardSize = size;
         game = gl;
     }// end setInfo
+
+    /* playColumn
+     * Purpose - chooses a column to play at and return it
+     *          It must be defensive or offensive or play as it likes
+     *          depending on the situation
+     * @param col - column the human player played at last
+     */
+    private int playColumn(int col)
+    {
+        // todo: write AI Code
+        return 0;
+    }// end playColumn
 }//class AIPlayer
