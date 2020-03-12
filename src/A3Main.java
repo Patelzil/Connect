32bd0b885a283ded5bc8186e722a2d49d076aa1a
Patelzil <58107277+Patelzil@users.noreply.github.com>
2020-03-12 17:11:23 -0500

    Main method comment

--- src/A3Main.java
+++ src/A3Main.java
@@ -1,3 +1,18 @@
+//-----------------------------------------
+// NAME		        : Patel Zil
+// STUDENT NUMBER	: 7876456
+// COURSE		    : COMP 2150
+// INSTRUCTOR	    : Mike Domaratzki
+// ASSIGNMENT	    : assignment 3
+// QUESTION     	: question 1
+//
+// REMARKS: enable a user to play a game of “Connect”, where players
+//          alternate dropping coloured playing pieces into a grid to
+//          get four of their pieces in a line (horizontal, diagonal or vertical)
+//          to win
+//
+//-----------------------------------------
+
 public class A3Main
 {
     public static void main(String[] args)
